37f31b17f2ef86dde73d91cf0e877870b79f8082
GhaidaaShtayeh <GhaidaaShtayeh@hotmail.com>
2022-03-25 12:22:49 +0300

    MVVM Files Structure

---
+++ CCSN/CCSN/CCSN/Models/Appoitment.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CCSN.Models
+{
+    internal class Appoitment
+    {
+    }
+}
---
+++ CCSN/CCSN/CCSN/Services/PatientService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CCSN.Services
+{
+    internal class PatientService
+    {
+    }
+}
