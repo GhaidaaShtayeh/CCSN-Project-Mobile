cda071e37c8910a7a6815fa187db5d4612a6a80f
ahedhamad <99614732+ahedhamad@users.noreply.github.com>
2022-03-28 20:05:56 +0300

    Update ScheduleAppointmentPageModelView.cs

--- CCSN/CCSN/CCSN/ViewModels/ScheduleAppointmentPageModelView.cs
+++ CCSN/CCSN/CCSN/ViewModels/ScheduleAppointmentPageModelView.cs
@@ -1,9 +1,10 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+
 namespace CCSN.ViewModels
 {
     internal class ScheduleAppointmentPageModelView : BindableObject
